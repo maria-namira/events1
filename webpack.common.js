fa764a2fb80da99aaf54f235dab0bdd9f18e7fe3
Maria <63401949+maria-namira@users.noreply.github.com>
2023-01-18 17:58:16 +0300

    Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -11,10 +11,12 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
+           test: /\.scss$/,
+        use: [
+            "style-loader", 
+            "css-loader",  
+            "sass-loader", 
+          ],
         },
       },
       {
